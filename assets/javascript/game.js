25cee4fba2994332f71a1ccf6bc467d86d0bc0be
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-05-31 15:38:54 -0400

    Can now check if user entry is present.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,20 @@
 var yourGuesses = []
+var secretWord = "tiki"
 
+// Give JavaScript a function to execute when onkeyup event fires.
+document.onkeyup = function(event) {
+    userText.textContent = event.key;
+       	if (userText.textContent != "Meta"){
+        	yourGuesses.push(userText.textContent);
+        	alert(secretWord);
+        	alert(userText.textContent);
+        	var n = secretWord.search(userText.textContent);
+        	alert(n);
+        	if (n >= 0){
+        		alert("Letter is present");
+        	} else {
+        		alert("Letter is not present");
+        	}
+        } 
+    document.getElementById("already_guessed").innerHTML = yourGuesses.join(" ");
+};
