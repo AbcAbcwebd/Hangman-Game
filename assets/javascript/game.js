9eef47608071b6f9f83720f40bbb3ae00089f37d
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-05-31 16:09:58 -0400

    Got typing functionality working again.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,36 @@
-var yourGuesses = []
-var secretWord = "tiki"
+var yourGuesses = [];
+var secretWord = "tiki";
+//var letterCount = secretWord.length;
+//var mysteryDisplay = ""
+//var l = -1;
 
 // Give JavaScript a function to execute when onkeyup event fires.
 document.onkeyup = function(event) {
     userText.textContent = event.key;
-       	if (userText.textContent != "Meta"){
-        	yourGuesses.push(userText.textContent);
-        	alert(secretWord);
-        	alert(userText.textContent);
-        	var n = secretWord.search(userText.textContent);
-        	alert(n);
-        	if (n >= 0){
-        		alert("Letter is present");
-        	} else {
-        		alert("Letter is not present");
-        	}
-        } 
-    document.getElementById("already_guessed").innerHTML = yourGuesses.join(" ");
-};
+       //	if (userText.textContent != "Meta"){
+       // 	yourGuesses.push(userText.textContent);
+        	//var n = secretWord.search(userText.textContent);
+        	//alert(n);
+        	//if (n >= 0){
+        		//Letter is present
+        		//updateMysteryDisplay(n)
+        //	} else {
+        		//Letter is not present
+        	//}
+       // } //else {
+        	//updateMysteryDisplay("meta")
+      //  }
+   // document.getElementById("already_guessed").innerHTML = yourGuesses.join(" ");
+}
+
+function updateMysteryDisplay(l){
+	//if l = 0 {
+		//mysteryDisplay.setText("")
+	//	for (i = 0; i < letterCount; i++) { 
+    		//mysteryDisplay = mysteryDisplay + "_"
+	//	}
+	//}
+
+	//document.getElementById("mystery-word-display").innerHTML = mysteryDisplay;
+
+}
