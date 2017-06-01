fe97ce8767a871f30d2411f36c69a5cb869d122d
Owen Essen <owenessen@Owens-MacBook-Air.local>
2017-06-01 13:42:43 -0400

    Spaces now turn to letter when guessed, unless there are multiple of each letter.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,35 +3,42 @@ var secretWord = "tiki";
 var letterCount = secretWord.length;
 var mysteryDisplay = ""
 var l = -2;
+var wordArray = secretWord.split("");
+var displayArray = [];
 
-// Give JavaScript a function to execute when onkeyup event fires.
+// Onkey up function
 document.onkeyup = function(event) {
     userText.textContent = event.key;
-       	if (userText.textContent != "Meta"){
+    updateMysteryDisplay(1);
+       	if ((userText.textContent != "Meta") && ((userText.textContent).match(/[a-z]/i))){
         	yourGuesses.push(userText.textContent);
-        	var n = secretWord.search(userText.textContent);
+        	var n = wordArray.indexOf(userText.textContent);
         	if (n >= 0){
         		//Letter is present
-        		updateMysteryDisplay(n)
+        		updateMysteryDisplay(n);
         	} else {
         		//Letter is not present
         	}
-        } else {
+        } else if (userText.textContent === "Meta") {
         	updateMysteryDisplay(-3)
         }
     document.getElementById("already_guessed").innerHTML = yourGuesses.join(" ");
 }
 
+// Function to iniate the spaces and letters mystery display.
+function initiateMysteryDisplay() {
+    displayArray = [];
+    for (x = 0; x < letterCount; x++) { 
+            displayArray.push("_ ");
+        }
+    document.getElementById("mystery-word-display").innerHTML = displayArray.join(" ");
+}
+
+// Function to update the spaces and letters mystery display.
 function updateMysteryDisplay(l){
-	if (l <= -1) {
-		mysteryDisplay = ""
-		for (i = 0; i < letterCount; i++) { 
-    		mysteryDisplay = mysteryDisplay + "_ "
-		}
-	} else if (l <= 0) {
-		
+	if (l >= 0) {
+		var i = wordArray[l];
+        displayArray[l] = i;
+        document.getElementById("mystery-word-display").innerHTML = displayArray.join(" ");
 	}
-
-	document.getElementById("mystery-word-display").innerHTML = mysteryDisplay;
-
 }
